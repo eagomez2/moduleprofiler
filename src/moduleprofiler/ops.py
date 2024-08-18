689da1d03a9de6e9782b308bcf5612b7258d515b
Esteban Gómez <eagomez@estebans-macbook-pro.home>
2024-08-18 17:38:08 +0200

    Add _conv2d_ops_fn

--- src/moduleprofiler/ops.py
+++ src/moduleprofiler/ops.py
@@ -55,20 +55,21 @@ def _conv1d_ops_fn(
     batch_size = 1 if len(x0.size()) == 2 else x0.size(0)
 
     # Compute input length of a single channel
-    x0_len = x0.size(-1)
+    # x0_len = x0.size(-1)
 
     # Avoid invalid operations caused by incompatible types
-    padding = module.padding[0]
-    dilation = module.dilation[0]
+    # padding = module.padding[0]
+    # dilation = module.dilation[0]
     kernel_size = module.kernel_size[0]
-    stride = module.stride[0]
+    # stride = module.stride[0]
 
     # Compute output length
     # NOTE: It can also be directly derived from the module's output
-    y0_len = (
-        ((x0_len + 2 * padding - dilation * (kernel_size - 1) - 1) / stride)
-        + 1
-    )
+    # y0_len = (
+    #     ((x0_len + 2 * padding - dilation * (kernel_size - 1) - 1) / stride)
+    #     + 1
+    # )
+    y0_len = output.numel()
 
     # Compute number of filters
     num_filters = ((module.in_channels * module.out_channels) / module.groups)
@@ -85,6 +86,49 @@ def _conv1d_ops_fn(
     )
 
     total_ops = batch_size * (num_filters * ops_per_filter + aggr_ops)
+
+    return int(total_ops)
+
+
+def _conv2d_ops_fn(
+        module: nn.Conv2d,
+        input: Tuple[torch.Tensor],
+        output: torch.Tensor
+) -> int:
+    # Get input
+    x0 = input[0]
+
+    # Get batch size
+    batch_size = 1 if x0.ndim == 2 else x0.size(0)
+
+    # Compute input length of a single channel
+    # x0_len = x0.size(-2) * x0.size(-1)
+
+    # Avoid invalid operations caused by incompatible types
+    kernel_size = (
+        module.kernel_size[0] if isinstance(module.kernel_size, int)
+        else module.kernel_size[0] * module.kernel_size[1]
+    )
+
+    # Compute output length
+    # NOTE: It can also be directly derived from the model's output
+    y0_len = output.numel()
+
+    # Compute number of filters
+    num_filters = ((module.in_channels * module.out_channels) / module.groups)
+
+    # Compute operations per filter
+    ops_per_filter = y0_len * (2 * kernel_size - 1)
+
+    # Compute number of aggregation operations
+    aggr_ops_bias = (0 if module.bias is not None else 1)
+    aggr_ops = (
+        y0_len
+        * module.out_channels
+        * ((module.in_channels / module.groups) - aggr_ops_bias)
+    )
+
+    total_ops = batch_size * (num_filters * ops_per_filter * aggr_ops)
 
     return int(total_ops)
 
@@ -310,6 +354,7 @@ def _get_default_ops_map() -> dict:
         nn.Identity: _identity_ops_fn,
         nn.Linear: _linear_ops_fn,
         nn.Conv1d: _conv1d_ops_fn,
+        nn.Conv2d: _conv2d_ops_fn,
         nn.GRUCell: _grucell_ops_fn,
         nn.GRU: _gru_ops_fn,
         nn.LSTMCell: _lstmcell_ops_fn,
